d6678baad0ccfcd1c97a656d2cf7fab0ed1dca68
Justin Daniel <justinddaniel@gmail.com>
2017-10-20 01:17:48 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(name = nil, weight = nil, height = nil)
+    @name = name
+    @weight = weight
+    @height = height
+    PIRATES << self
+  end
+
+  def self.all 
+    PIRATES
+  end
+
+  def self.clear
+    PIRATES = []
+  end
+  
+  
 end
